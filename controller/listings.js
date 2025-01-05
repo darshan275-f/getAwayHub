4c734c399e6bd9915b76dfed0ebb29a609472a1f
Darshan Jomaling Patil <darshanjomalingpatil@gmail.com>
2025-01-05 13:09:46 +0530

    Delete controller directory

--- controller/listings.js
+++
@@ -1,7 +0,0 @@
-const Listing=require("../models/listing");
-
-module.exports.index=async (req,res)=>{
-    let listing=await Listing.find({});
-    // console.log(listing);
-    res.render("./listings/index.ejs",{listing});
-}
